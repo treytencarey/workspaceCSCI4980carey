d736a82fed3fe9303e2649950424ee644df49d62
ADMIN\Treyten <treytencarey@gmail.com>
2018-11-01 17:10:15 -0500

    1101 in-class assignment

---
+++ runtime-project1101-zest-ast-type-Carey/target-project-1101.zip_expanded/project001/src/pkg1/A.java
@@ -0,0 +1,11 @@
+package pkg1;
+
+public class A {
+	void ma1(String str1) {
+	}
+
+	int ma2(String str2, int i1) {
+		System.out.println(i1);
+		return 0;
+	}
+}
---
+++ workspaceCSCI4980carey/project1101-zest-ast-type-Carey/src/handler/UpdateGraphViewHandler.java
@@ -0,0 +1,31 @@
+/*
+ * @(#) ASTZestHandler.java
+ *
+ * Copyright 2015-2018 The Software Analysis Laboratory
+ * Computer Science, The University of Nebraska at Omaha
+ * 6001 Dodge Street, Omaha, NE 68182.
+ */
+package handler;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+
+import analysis.ProjectAnalyzer;
+import view.SimpleZestGraphView;
+
+public class UpdateGraphViewHandler {
+   @Execute
+   public void execute(EPartService service) throws CoreException {
+      System.out.println("[DBG] UpdateGraphViewHandler");
+      MPart findPart = service.findPart(SimpleZestGraphView.SIMPLEZESTVIEW);
+      System.out.println(MPart.class);
+      if (findPart != null && findPart.getObject() instanceof SimpleZestGraphView) {
+    	 System.out.println("[DBG] Found part");
+         ProjectAnalyzer analyzer = new ProjectAnalyzer();
+         analyzer.analyze();
+         ((SimpleZestGraphView) findPart.getObject()).update();
+      }
+   }
+}
