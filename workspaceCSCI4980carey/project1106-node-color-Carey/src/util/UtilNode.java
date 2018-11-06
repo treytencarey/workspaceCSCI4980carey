90559a5d440d23a2bfed830c3af28b67b88ea0d8
ADMIN\Treyten <treytencarey@gmail.com>
2018-11-06 17:29:51 -0600

    1106 in-class assignment

---
+++ workspaceCSCI4980carey/project1106-node-color-Carey/src/util/UtilNode.java
@@ -0,0 +1,71 @@
+package util;
+
+import java.util.EventObject;
+import java.util.List;
+
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.zest.core.widgets.Graph;
+import org.eclipse.zest.core.widgets.GraphNode;
+
+import graph.model.GClassNode;
+import graph.model.GMethodNode;
+import graph.model.GNode;
+import graph.model.GNodeType;
+import graph.model.GPackageNode;
+import graph.provider.GLabelProvider;
+
+public class UtilNode {
+   public static void resetDstNode(GraphNode graphNode, GNode node) {
+      if (graphNode == null || graphNode.isDisposed() || !(node instanceof GClassNode))
+         return;
+      graphNode.setForegroundColor(ColorConstants.black);
+      graphNode.setBackgroundColor(ColorConstants.lightBlue);
+      graphNode.setBorderColor(ColorConstants.lightBlue);
+      graphNode.setHighlightColor(ColorConstants.lightBlue);
+      graphNode.setBorderHighlightColor(ColorConstants.lightBlue);
+      node.setNodeType(GNodeType.InValid);
+   }
+
+   public static void resetPackageNode(GraphNode graphNode, GNode node) {
+      if (graphNode == null || graphNode.isDisposed() || !(node instanceof GPackageNode)) {
+         return;
+      }
+      // TODO: Class Exercise
+      GLabelProvider prov = new GLabelProvider();
+      graphNode.setForegroundColor(prov.getForegroundColour(node));
+      graphNode.setBackgroundColor(prov.getBackgroundColour(node));
+      graphNode.setBorderColor(prov.getBorderColor(node));
+      graphNode.setHighlightColor(prov.getNodeHighlightColor(node));
+      graphNode.setBorderHighlightColor(prov.getBorderHighlightColor(node));
+      node.setNodeType(GNodeType.InValid);
+   }
+
+   public static boolean isPackageNode(EventObject e) {
+      Object obj = e.getSource();
+      Graph graph = (Graph) obj;
+
+      boolean result = false;
+      // TODO: Class Exercise
+      result = (!graph.getSelection().isEmpty() && //
+        graph.getSelection().get(0) instanceof GraphNode && //
+        ((GraphNode) graph.getSelection().get(0)).getData() instanceof GPackageNode);
+      return result;
+   }
+
+   public static boolean isClassNode(EventObject e) {
+      Object obj = e.getSource();
+      Graph graph = (Graph) obj;
+
+      return (!graph.getSelection().isEmpty() && //
+            graph.getSelection().get(0) instanceof GraphNode && //
+            ((GraphNode) graph.getSelection().get(0)).getData() instanceof GClassNode);
+   }
+
+   public static boolean isMethodNode(EventObject e) {
+      Object obj = e.getSource();
+      Graph instGraph = (Graph) obj;
+      List<?> selection = instGraph.getSelection();
+      return ((!selection.isEmpty() && selection.get(0) instanceof GraphNode) && //
+            (((GraphNode) selection.get(0)).getData() instanceof GMethodNode));
+   }
+}
